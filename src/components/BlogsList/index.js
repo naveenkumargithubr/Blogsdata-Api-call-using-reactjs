cf22b541b8f7a24d33f59a211dda892f172e8f18
naveen kumar <112088541+naveenkumargithubr@users.noreply.github.com>
2023-09-12 12:36:33 +0530

    Update index.js

--- src/components/BlogsList/index.js
+++ src/components/BlogsList/index.js
@@ -1,32 +1,29 @@
-import {Component} from 'react'
-
 import Loader from 'react-loader-spinner'
-
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-
+import {Component} from 'react'
 import BlogItem from '../BlogItem'
 
 import './index.css'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
-/* const blogsData = [
-  {
-    id: 1,
-    title: 'Blog 1',
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
-    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-    author: 'Author Name',
-    topic: 'React.js',
-  },
-  {
-    id: 2,
-    title: 'Blog 2',
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
-    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-    author: 'Author Name',
-    topic: 'React.js',
-  },
-]
-*/
+// Dummy data
+// const blogsData = [
+//   {
+//     id: 1,
+//     title: 'Blog 1',
+//     imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
+//     avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//     author: 'Author Name',
+//     topic: 'React.js',
+//   },
+//   {
+//     id: 2,
+//     title: 'Blog 2',
+//     imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
+//     avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//     author: 'Author Name',
+//     topic: 'React.js',
+//   },
+// ]
 
 class BlogsList extends Component {
   state = {blogsData: [], isLoading: true}
@@ -35,9 +32,11 @@ class BlogsList extends Component {
     this.getBlogsData()
   }
 
+  // making api call
   getBlogsData = async () => {
     const response = await fetch('https://apis.ccbp.in/blogs')
     const data = await response.json()
+    // here we convert backend snake_case to frontend camelCase
     const updatedData = data.map(eachItem => ({
       id: eachItem.id,
       title: eachItem.title,
@@ -46,15 +45,16 @@ class BlogsList extends Component {
       author: eachItem.author,
       topic: eachItem.topic,
     }))
+    // now update the the data replace the dummy data to api data
     this.setState({blogsData: updatedData, isLoading: false})
   }
 
   render() {
-    const {blogsData} = this.state
+    const {blogsData} = this.state // get the updated api data to the render method
     const {isLoading} = this.state
-
     return (
       <div className="blog-list-container">
+        {/* finally iterate the each item using map and display the results */}
         {isLoading ? (
           <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
         ) : (
